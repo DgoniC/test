078c22333c6d44f813f977c1e09699f7797075af
Dgoni <ex@mail.ru>
2019-04-23 16:46:42 +0500

    Это первый коммит

---
+++ script.js
@@ -0,0 +1,54 @@
+// var massive = {};
+
+// massive = {
+// 	name: 'john',
+// 	age: 15,
+// 	isStillAlive: false
+// }
+
+// console.log(massive.age);
+
+// var arr = ['car', 'bike', 'doll'];
+// console.log(arr[1]);
+
+// var conf = confirm('are you here?');
+
+// console.log( typeof(conf));
+
+// (isCheked || isClose) || - или
+// (isCheked && isClose) && - и
+
+// var money = prompt('Ваш бюджет?');
+// var name = prompt('Введите название вашего магазина');
+
+
+// var type = prompt('какой тип товара будем продовать?', 'груши');
+// var type2 = prompt('какой тип товара будем продовать?', 'яблоки');
+// var type3 = prompt('какой тип товара будем продовать?', 'бананы');
+
+/*mainList = {
+	summ: 10000,
+	shopName: 'crystal',
+	shopGoods: ['table', 'phone', type, type2, type3],
+	open: true,
+	employers: {
+		john: 'john',
+		darek: 'darek'
+	}
+
+
+ }
+ */
+
+// alert('Ваш бюджет за один день' + 1000/30);
+
+
+// var num = 33721;
+
+
+// console.log( 3*3*7*2*1);
+
+alert(126 ** 3);
+
+
+// console.log(mainList);
